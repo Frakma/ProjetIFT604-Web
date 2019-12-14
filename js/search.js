eb869a81175b7b30869b481b8b9899036a5195a7
Léo <leopec@hotmail.fr>
2019-12-14 13:55:40 -0500

    float to int

--- js/search.js
+++ js/search.js
@@ -104,7 +104,7 @@ function makeSearch()
     var searchParams = {
       center: map.getCenter().lat()+","+map.getCenter().lng(),
       date:dateDebutText+"-"+dateFinText,
-      distance:metersPerPx*$("#div-map")[0].clientWidth,
+      distance:Math.floor(metersPerPx*$("#div-map")[0].clientWidth),
       keywords:$("#keywordsInput").val()
     }
 
